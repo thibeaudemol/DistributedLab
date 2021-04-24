de441ec974dc72e0cade29cd7a6279d9bc6dfa96
thibeaudemol <thibeau.demol@student.uantwerpen.be>
2021-04-24 15:34:06 +0200

    working on feature2

--- src/be/uantwerpen/fti/ei/Main.java
+++ src/be/uantwerpen/fti/ei/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
     }
+
+    public void feature2(){
+        System.out.println("ERROR 404");
+
+    }
 }
